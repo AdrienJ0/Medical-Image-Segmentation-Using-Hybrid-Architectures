ee7addac0b7ee066b276b2e5e8fcbb2133799156
Adrien Junior TCHUEM TCHUENTE <87979922+AdrienJ0@users.noreply.github.com>
2024-05-28 23:01:20 +0200

    Delete TransUNet-main directory

--- TransUNet-main/datasets/download_dataset.py
+++
@@ -1,9 +0,0 @@
-import synapseclient 
-import synapseutils 
- 
-syn = synapseclient.Synapse() 
-syn.login('Adrien_J','H9NYS6A}Ch)a;V&') 
-averaged_testing_images = synapseutils.syncFromSynapse(syn, 'syn10284975') 
-averaged_training_images = synapseutils.syncFromSynapse(syn, 'syn10285054') 
-averaged_training_labels = synapseutils.syncFromSynapse(syn, 'syn10285076') 
-
